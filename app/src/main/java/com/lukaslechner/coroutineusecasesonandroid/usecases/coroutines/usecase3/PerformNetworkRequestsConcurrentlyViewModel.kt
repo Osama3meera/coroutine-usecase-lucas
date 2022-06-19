5de382dcc73b9bf3f18d99bb394785635bbb1fd4
o.hasan <osama.h@genie-soft.com>
2022-06-19 17:07:17 +0300

    async case added

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
@@ -1,7 +1,12 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase3
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class PerformNetworkRequestsConcurrentlyViewModel(
     private val mockApi: MockApi = mockApi()
@@ -9,9 +14,42 @@ class PerformNetworkRequestsConcurrentlyViewModel(
 
     fun performNetworkRequestsSequentially() {
 
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            try {
+                val oreoFeatures = mockApi.getAndroidVersionFeatures(27)
+                val pieFeatures = mockApi.getAndroidVersionFeatures(28)
+                val tenFeatures = mockApi.getAndroidVersionFeatures(29)
+
+                val versionFeatures = listOf(oreoFeatures, pieFeatures, tenFeatures)
+                uiState.value = UiState.Success(versionFeatures)
+            } catch (exception: Exception) {
+                uiState.value = UiState.Error("Network Request failed")
+            }
+
+        }
     }
 
     fun performNetworkRequestsConcurrently() {
+        uiState.value = UiState.Loading
+        val oreoFeaturesDeferred = viewModelScope.async {
+            mockApi.getAndroidVersionFeatures(27)
+        }
+        val pieFeaturesDeferred = viewModelScope.async {
+            mockApi.getAndroidVersionFeatures(28)
+        }
+        val tenFeaturesDeferred = viewModelScope.async {
+            mockApi.getAndroidVersionFeatures(29)
+        }
 
+        viewModelScope.launch {
+            try {
+
+                val versionsFeatures = awaitAll(oreoFeaturesDeferred, pieFeaturesDeferred, tenFeaturesDeferred)
+                uiState.value = UiState.Success(versionsFeatures)
+            } catch (exception: Exception) {
+                uiState.value = UiState.Error("Something went wrong!")
+            }
+        }
     }
 }
